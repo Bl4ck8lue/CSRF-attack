3c5171055cf1ea8184355544dc48fdb7d8511559
ilya.ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-05-18 10:56:01 +0300

    CSRF-attack

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("welcome.html")
+	if err != nil {
+		http.Error(w, "Could not open HTML file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	htmlData, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Could not read HTML file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(htmlData)
+}
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", CORS(formHandler))
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
